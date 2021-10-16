1c8e3b766c5ceae2d0a4c7e87bd0a3c76e570a92
김주희 <rlawn97@gmail.com>
2021-10-16 13:13:39 +0900

    소켓 start

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 //express 모듈 불러오기
-const express = require("express");
+var express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 //express 사용
-const app = express();
+var app = express();
+
+// socket
+// const server = require("http").createServer(app);
+// const io = require("socket.io").listen(server);
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  allowEIO3: true,
+});
+
+httpServer.listen(8080);
+// var server = require("http").Server(app);
+// var io = require("socket.io")(server);
 
 // Middlewares
 app.use(cors());
@@ -49,3 +64,28 @@ app.get("/", (req, res) => {
 
 // http listen port 생성 서버 실행
 app.listen(4000, () => console.log("Listening on port 4000"));
+
+connections = [];
+// server.listen(5000);
+console.log("Socket Server is running...");
+
+io.sockets.on("connection", function (socket) {
+  connections.push(socket);
+  console.log("Connect: %s sockets are connected", connections.length);
+
+  // disconnect
+  socket.on("disconnect", function (data) {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("Discconect: %s sockets are connected", connections.length);
+  });
+
+  socket.on("NodeJS Server Port", function (data) {
+    console.log(data);
+    io.sockets.emit("iOS Client Port", { message: "h i ios client!" });
+  });
+  socket.on("event1", function (data) {
+    console.log(data);
+    io.sockets.emit("e1", { message: data });
+  });
+  // socket.on()
+});
