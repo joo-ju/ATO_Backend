538df861a1c03cace60a3bc35e7026cfe87daea1
raaang <flylikew@naver.com>
2021-10-29 22:51:41 +0900

    change limit of whole files

--- src/index.js
+++ src/index.js
@@ -71,6 +71,9 @@ app.use("/user", userRoute);
 
 app.use("/goods", goodsRoute);
 app.use(express.static("public"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
+
 
 app.use("/wallet", walletRoute);
 
