34fadb11fa82f0287946891972e12938ee72fc8e
raaang <flylikew@naver.com>
2021-10-31 18:01:41 +0900

    update wallet history

--- src/routes/wallet.js
+++ src/routes/wallet.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Wallet = require("../model/wallet");
+const WalletHistory = require("../model/walletHistory");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -22,39 +23,44 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.get("/history", async (req, res) => {
+  try {
+    const walletHistory = await WalletHistory.find();
+    res.json(walletHistory);
+
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// post wallet and wallet history
 router.post("/", async (req, res) => {
   const wallet = new Wallet({
     userId: req.body.userId,
-    balance: req.body.balance,
   });
 
   wallet
     .save()
     .then((data) => {
-      res.json(data);
+      // res.json(data);
+      console.log(data);
     })
     .catch((err) => {
       res.json({ message: err });
     });
-});
 
-router.put("/updateWallet", async (req, res) => {
-  const updatedWallet = Wallet.updateOne(
-    { _id: req.body.id },
-    {
-      $set: {
-        userId: req.body.userId,
-        balance: req.body.balance,
-        updateTime: Date.now(),
-      },
-    },
-    {
-      upsert: true,
-      new: true,
+  const walletHistory = new WalletHistory({
+    userId: wallet.userId,
+    walletId: wallet._id,
+    content: {
+      cost: wallet.balance,
+      balance: wallet.balance,
+      type: "charge"
     }
-  ).exec();
-  
-  updatedWallet
+  });
+
+  walletHistory
+    .save()
     .then((data) => {
       res.json(data);
       console.log(data);
@@ -62,6 +68,46 @@ router.put("/updateWallet", async (req, res) => {
     .catch((err) => {
       res.json({ message: err });
     });
+});
+
+// update wallet and wallet history
+router.put("/updateWallet", async (req, res) => {
+  try {
+    const wallet = await Wallet.findOne({ userId: req.body.userId });
+    const balance = wallet.balance + req.body.cost;
+
+    const updatedWalletHistory = await WalletHistory.updateOne(
+      { userId: req.body.userId },
+      {
+        $push: {
+          content: {
+            cost: req.body.cost,
+            balance: balance,
+            type: req.body.type,
+          },
+        },
+      }
+    ).exec();
+    
+    // res.json(updatedWalletHistory);
+    console.log(updatedWalletHistory);
+
+    const updatedWallet = await Wallet.updateOne(
+      {userId:req.body.userId},
+      {
+        $set: {
+          balance: balance,
+          updatedTime: Date.now()
+        }
+      }
+    ).exec();
+    
+    res.json(updatedWallet);
+    console.log(updatedWallet);
+  
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 router.delete("/:id", async (req, res) => {
