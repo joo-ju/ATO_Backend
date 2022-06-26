281fe4fe6498dd4960abd04f0f290f5864a99fc2
raaang <flylikew@naver.com>
2022-06-26 23:13:29 +0900

    Feat: Event API 구현

--- src/model/event.js
+++ src/model/event.js
@@ -6,10 +6,18 @@ const eventSchema = new Schema({
   categoryId: { type: String }, // 카테고리(생일 등)
   discription: { type: String }, // 행사 설명
   sellerId: { type: String }, // 주최자 ID
+  buyerId: [{ type: String }], // 구매자 ID
+  state: { type: String, default: "판매중" }, // 판매중, 예약 등 상태
   age: { type: Number }, // 연령 제한(_세 이상)
   price: { type: Number }, // 결제 금액
   score: { type: Number, default: 0 }, // 별점 점수
   count: { type: Number, default: 0 }, // 후기 갯수
+  wishCount: { type: Number, default: 0 }, // 찜 갯수
+  wishUser: [],
+  chat: { type: Number, default: 0 }, // 채팅 갯수
+  review: { type: Number, default: 0 }, // 채팅 갯수
+  image: [{ type: String, required: true }], // 이미지
+  tags: [], // 태그
   showTime: [
     {
       dateTime: Date, // 행사 날짜
--- src/routes/event.js
+++ src/routes/event.js
@@ -1,13 +1,67 @@
 const express = require("express");
 const Event = require("../model/event");
 const router = express.Router();
+const multer = require("multer");
 
-// router.get("/", (req, res) => {
-//   res.send("event get");
-// });
+// image upload directory
+const storage = multer.diskStorage({
+  destination: "./images/event",
+  filename: (req, file, cb) => {
+    // not working on new Date().toISOString()
+    cb(null, Date.now() + "_" + file.originalname);
+  },
+});
 
+let fileFilter = function (req, file, cb) {
+  var allowedMimes = ["image/jpeg", "image/jpg", "image/pjpeg", "image/png"];
+  if (allowedMimes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(
+      {
+        success: false,
+        message: "Invalid file type. Only jpg, png image files are allowed.",
+      },
+      false
+    );
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  // limits: { fileSize: 100 * 1024 * 1024 },
+  limits: { fileSize: 200 * 1024 * 1024 },
+  fileFilter: fileFilter,
+});
+
+// All events
+router.get("/", async (req, res) => {
+  try {
+    console.log("get all events");
+    const event = await Event.find();
+    res.json(event);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Find One Event
+router.get("/:eventId", async (req, res) => {
+  try {
+    const event = await Event.findById({ _id: req.params.eventId });
+    res.json(event);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// can post 1-10 images
+// router.post("/", upload.array("image", 10), async (req, res) => {
 router.post("/", async (req, res) => {
   console.log(req.body);
+
+  var filenames = [];
+
   const event = new Event({
     name: req.body.name,
     categoryId: req.body.categoryId,
@@ -15,8 +69,8 @@ router.post("/", async (req, res) => {
     sellerId: req.body.sellerId,
     age: req.body.age,
     price: req.body.price,
-    score: req.body.score,
-    count: req.body.count,
+    showTime: req.body.showTime,
+    image: filenames,
   });
 
   event
@@ -30,35 +84,162 @@ router.post("/", async (req, res) => {
     });
 });
 
-// Specific Event
-router.get("/:eventId", async (req, res) => {
+// Post only images
+router.post("/image", upload.array("image", 10), async (req, res) => {
+  console.log("req.files : ", req.files);
+  console.log("req : ", req);
+  console.log("req.body : ", req.body);
+  var filenames = [];
+  req.files.forEach((item) => {
+    filenames.push(item.filename);
+  });
+
+  const event = new Event({
+    image: filenames,
+  });
+
+  event
+    .save()
+    .then((data) => {
+      res.json(data);
+      console.log(data);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+      console.log(err);
+    });
+});
+
+// Display One Image
+router.get("/image/:image", async (req, res) => {
   try {
-    const event = await Event.findById(req.params.eventId);
-    res.json(event);
+    console.log(req.url.split("/")[2]);
+    res.sendFile(req.url.split("/")[2], { root: "./images/event/" });
   } catch (err) {
+    res.json({
+      error: false,
+      message: err,
+      data: req.url,
+    });
+  }
+});
+
+// Update Event Only Images
+router.put("/update/image/:eventId", upload.array("image", 10), async (req, res) => {
+  try {
+    var filenames = [];
+    req.files.forEach((item) => {
+      filenames.push(item.filename);
+    });
+
+    const updatedEvent = Event.updateOne(
+      { _id: req.params.eventId },
+      {
+        $set: {
+          image: filenames
+        }
+      },
+      {
+        upsert: true,
+        new: true
+      }
+    )
+    .exec()
+    .then((data) => {
+      res.json(data);
+      console.log(data);w
+    });
+
+    // res.json(updatedEvent);
+    // console.log(updatedEvent);
+  } catch (err) {
+    console.log(err);
     res.json({ message: err });
   }
 });
-router.get("/", async (req, res) => {
+
+// Delete Event Only Images
+router.put("/image/delete/:eventId", async (req, res) => {
   try {
-    const event = await Event.find();
-    res.json(event);
+    const updatedEvent = await Event.updateOne(
+      { _id: req.params.eventId },
+      {
+        $pull: {
+          image: req.body.image,
+        }
+      },
+      {
+        upsert: true,
+        new: true
+      }
+    ).exec();
+    res.json(updatedEvent);
+    console.log(updatedEvent);
   } catch (err) {
     res.json({ message: err });
   }
 });
 
 // Update a Event
-router.put("/:eventId", async (req, res) => {
+router.put("/update/:eventId", async (req, res) => {
   try {
     const updatedEvent = Event.updateOne(
       { _id: req.params.eventId },
       {
-        $set: { name: req.body.name },
+        $set: {
+          name: req.body.name,
+          categoryId: req.body.categoryId,
+          discription: req.body.discription,
+          sellerId: req.body.sellerId,
+          age: req.body.age,
+          price: req.body.price,
+          showTime: req.body.showTime,
+          updateTime: Date.now()
+        }
+      },
+      {
+        upsert: true,
+        new: true
       }
-    );
-    res.json(updatedEvent);
+    )
+    .exec()
+    .then((data) => {
+      res.json(data);
+      console.log(data);
+    });
+
+    // res.json(updatedEvent);
+    // console.log(updatedEvent);
   } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Update Event Only state
+router.put("/update/state/:eventId", async (req, res) => {
+  try {
+    const updatedEvent = Event.updateOne(
+      { _id: req.params.eventId },
+      {
+        $set: {
+          state: req.body.state
+        }
+      },
+      {
+        upsert: true,
+        new: true
+      }
+    )
+    .exec()
+    .then((data) => {
+      res.json(data);
+      console.log(data);
+    });
+
+    // res.json(updatedEvent);
+    // console.log(updatedEvent);
+  } catch (err) {
+    console.log(err);
     res.json({ message: err });
   }
 });
@@ -66,11 +247,105 @@ router.put("/:eventId", async (req, res) => {
 // Delete Event
 router.delete("/:eventId", async (req, res) => {
   try {
-    const removedEvent = await Event.remove({ _id: req.params.eventId });
+    const removedEvent = await Event.deleteOne({ _id: req.params.eventId });
     res.json(removedEvent);
   } catch (err) {
     res.json({ message: err });
   }
 });
+
+// Find All Sale Events of User
+router.get("/user/sale/all/:sellerId", async (req, res) => {
+  try {
+    console.log(req.params.sellerId);
+    const event = await Event.find({
+      sellerId: req.params.sellerId,
+      state: "판매중",
+    });
+
+    res.json(event);
+  } catch (err) {
+    res.json({
+      error: false,
+      message: err,
+      data: event,
+    });
+  }
+});
+
+// Find All Sold Events of User
+router.get("/user/sold/all/:sellerId", async (req, res) => {
+  try {
+    console.log(req.params.sellerId);
+    const event = await Event.find({
+      sellerId: req.params.sellerId,
+      state: "판매완료",
+    });
+
+    res.json(event);
+  } catch (err) {
+    res.json({
+      error: false,
+      message: err,
+      data: event,
+    });
+  }
+});
+
+// Find All Hidden Events of User
+router.get("/user/hiding/all/:sellerId", async (req, res) => {
+  try {
+    console.log(req.params.sellerId);
+    const goods = await Event.find({
+      sellerId: req.params.sellerId,
+      state: "예약중",
+    });
+
+    res.json(event);
+  } catch (err) {
+    res.json({
+      error: false,
+      message: err,
+      data: event,
+    });
+  }
+});
+
+// Find All Bought Events of User
+router.get("/user/buy/all/:buyerId", async (req, res) => {
+  try {
+    console.log(req.params.buyerId);
+    const event = await Event.find({
+      buyerId: req.params.buyerId,
+      state: "판매완료",
+    });
+
+    res.json(event);
+  } catch (err) {
+    res.json({
+      error: false,
+      message: err,
+      data: event,
+    });
+  }
+});
+
+// Find Sale or Booking Events
+router.get("/sale/all", async (req, res) => {
+  try {
+    const event = await Event.find({
+      $or: [{ state: "판매중" }, { state: "예약중" }],
+    });
+
+    res.json(event);
+  } catch (err) {
+    res.json({
+      error: false,
+      message: err,
+      data: event,
+    });
+  }
+});
+
 
 module.exports = router;
